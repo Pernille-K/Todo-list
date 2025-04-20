ef1d9e40a496b6b602e96052c8216e606b254ade
Pernille-K <pernilleklarpaas@outlook.com>
2025-04-20 20:43:15 +0200

    Custom titlebar
    Created a custom titlebar and custom window-control buttons

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
 const path = require("path");
 const { app, BrowserWindow, ipcMain } = require("electron");
 const fs = require("fs");
-const { globalShortcut } = require('electron');
+const { globalShortcut } = require("electron");
 
 let win;
 
-function createWindow() {
-    win = new BrowserWindow({
-      width: 550,
-      height: 600,
-      webPreferences: {
-        nodeIntegration: false,
-        contextIsolation: true,
-        preload: path.join(__dirname, "preload.js"),
-      },
-    });
-  
-    win.removeMenu();
-    win.loadFile("index.html");
-    
-    ipcMain.on("load-page", (event, page) => {
-      win.loadFile(page);
-    });
-  }
+require("electron-reload")(__dirname, {
+	electron: require(`${__dirname}/node_modules/electron`),
+});
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  });
-  
-  app.whenReady().then(() => {
-    createWindow();
-    app.on("activate", () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-      }
-    });
-    
-    globalShortcut.register('CommandOrControl+Shift+I', () => {
-      const wc = win.webContents;
-      if (wc.isDevToolsOpened()) {
-        wc.closeDevTools();
-      } else {
-        wc.openDevTools();
-      }
-    })
-  });
+function createWindow() {
+	win = new BrowserWindow({
+		width: 550,
+		height: 600,
+		// titleBarStyle: "hidden",
+		frame: false,
+		webPreferences: {
+			nodeIntegration: false,
+			contextIsolation: true,
+			preload: path.join(__dirname, "preload.js"),
+		},
+		...(process.platform !== "darwin" ? { titleBarOverlay: true } : {}),
+	});
+
+	win.removeMenu();
+	win.loadFile("index.html");
+
+	ipcMain.on("load-page", (event, page) => {
+		win.loadFile(page);
+	});
+}
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("minimize", () => {
+	win.minimize();
+});
+ipcMain.on("maximize", () => {
+	if (win.isMaximized()) {
+		win.unmaximize();
+	} else {
+		win.maximize();
+	}
+});
+ipcMain.on("close", () => {
+	win.close();
+});
+
+app.whenReady().then(() => {
+	createWindow();
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+
+	globalShortcut.register("CommandOrControl+Shift+I", () => {
+		const wc = win.webContents;
+		if (wc.isDevToolsOpened()) {
+			wc.closeDevTools();
+		} else {
+			wc.openDevTools();
+		}
+	});
+});
--- preload.js
+++ preload.js
@@ -1,7 +1,10 @@
 // Runs before the renderer process is loaded, and has access to both renderer globals (window and doocument) and a Node.js environment
 
-const {contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-    loadPage: (page) => ipcRenderer.send("load-page", page),
-  });
+	loadPage: (page) => ipcRenderer.send("load-page", page),
+	minimize: () => ipcRenderer.send("minimize"),
+	maximize: () => ipcRenderer.send("maximize"),
+	close: () => ipcRenderer.send("close"),
+});
