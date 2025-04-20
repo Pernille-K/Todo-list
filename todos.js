70c800fc4ec09eb85d0a49f85953db973c658278
Pernille-K <pernilleklarpaas@outlook.com>
2025-04-20 20:57:59 +0200

    Update background based on progress
    Creating functionality in todos.js where the background is updated based on the progress. Tracking this progress based on a prop isCompleted on each task

--- todos.js
+++ todos.js
@@ -39,6 +39,7 @@ function addTask() {
 	tasks.push({
 		name: String(taskName),
 		timestamp: timeAdded,
+		isCompleted: false,
 	});
 
 	addListener(taskName);
@@ -72,11 +73,17 @@ function iterateTasks(taskName) {
 	return null;
 }
 
+function checkComplete(task) {
+	return task.isCompleted == true;
+}
+
 function addListener(taskName) {
 	const checkBox = document.getElementById("checkbox_" + taskName);
 
 	checkBox.addEventListener("change", onChange);
+	updateOutput();
 }
+
 function onChange(e) {
 	const checkboxName = e.target.id;
 
@@ -86,12 +93,48 @@ function onChange(e) {
 
 	if (this.checked) {
 		currentListItem.style.textDecoration = "line-through";
+		currentTaskObj.isCompleted = true;
 	} else {
 		currentListItem.style.textDecoration = "none";
+		currentTaskObj.isCompleted = false;
 	}
 
+	updateOutput();
 }
 
+function updateOutput() {
+	console.log("output updated");
+	const outputElement = document.getElementById("output");
+	const noTasks = tasks.length;
+
+	const noCompleted = tasks.filter(checkComplete).length;
+
+	outputElement.innerText = ` ${noCompleted} / ${noTasks} completed`;
+
+	let proportionIndex = noTasks > 0 ? noCompleted / noTasks : 0;
+
+	checkAndChangeBackground(proportionIndex);
+}
+
+function checkAndChangeBackground(proportionIndex) {
+	console.log("er inni checkandchangebackground");
+	const root = document.documentElement;
+
+	// The thresholds array contains thresholds where each defines a max value and the corresponding colors for container and body
+	const thresholds = [
+		{ max: 0.25, containerColor: "#FF6961", bodyColor: "#ff9e99", detailsColor: "#ffe6e5" },
+		{ max: 0.5, containerColor: "#fab350", bodyColor: "#ffcc84", detailsColor: "#fce8cf" },
+		{ max: 0.75, containerColor: "#F8D66D", bodyColor: "#ffe79f", detailsColor: "#fff4d3" },
+		{ max: 1, containerColor: "#8CD47E", bodyColor: "#aeefa1", detailsColor: "#e5ffe0" },
+		{ max: Infinity, containerColor: "#e4c9ff", bodyColor: "#C78EFF", detailsColor: "#f6edff" },
+	];
+
+	const { containerColor, bodyColor, detailsColor } = thresholds.find((threshold) => proportionIndex <= threshold.max);
+
+	root.style.setProperty("--background-color-container", containerColor);
+	root.style.setProperty("--background-color-body", bodyColor);
+	root.style.setProperty("--details-color", detailsColor);
+}
 
 const tasks = [];
 let i = 1;
